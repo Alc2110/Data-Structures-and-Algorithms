a6ea0e732d2ee0696a4bec6b53cb8eec5aefbb5f
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-05-02 15:53:42 +1000

    add C# implementation of unordered linked list based symbol table

--- CsImplementations/Sandbox/Program.cs
+++ CsImplementations/Sandbox/Program.cs
@@ -7,6 +7,7 @@ using DataStructures.Stacks;
 using DataStructures.Trees;
 using DataStructures.Graphs;
 using DataStructures.Graphs.Algorithms;
+using DataStructures.SymbolTables;
 
 namespace Sandbox
 {
@@ -14,49 +15,31 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            /*
-            BinarySearchTree<int> bst = new BinarySearchTree<int>();
-            bst.Insert(1);
-            bst.Insert(6);
-            bst.Insert(4);
-            bst.Insert(5);
-            bst.Delete(4);
-            bst.Delete(3);
+            UnorderedLinkedListSymbolTable<int, string> st = new UnorderedLinkedListSymbolTable<int, string>();
+            st.Insert(1, "one");
+            st.Insert(2, "two");
+            st.Insert(3, "three");
 
-            Console.WriteLine("Contains 1: " + bst.Contains(1));
-            Console.WriteLine("Contains 4: " + bst.Contains(4));
-            Console.WriteLine("Contains 5: " + bst.Contains(5));
-            Console.WriteLine("Contains 6: " + bst.Contains(6));
-            Console.WriteLine("Contains 7: " + bst.Contains(7));
-            */
-            Graph graph = new Graph(7);
-            graph.AddEdge(0, 1);
-            graph.AddEdge(0, 2);
-            graph.AddEdge(0, 6);
-            graph.AddEdge(6, 4);
-            graph.AddEdge(4, 3);
-            graph.AddEdge(4, 5);
-            graph.AddEdge(3, 5);
-            graph.AddEdge(0, 5);
-            Console.Write(graph.ToString());
+            Console.WriteLine("IsEmpty: " + st.IsEmpty());
+            Console.WriteLine("Size: " + st.Size());
 
-            Graph graph2 = new Graph(graph);
+            Console.WriteLine(st.Get(1));
+            Console.WriteLine(st.Get(4));
+
+            st.Insert(4, "four");
+
+            Console.WriteLine(st.Get(4));
+
+            st.Delete(4);
+
+            Console.WriteLine(st.Get(4));
+
+            Console.WriteLine("Size: " + st.Size());
 
             Console.WriteLine("");
-            Console.Write(graph2.ToString());
-
-            //BreadthFirstPaths bfp = new BreadthFirstPaths(graph, 0);
-            /*
-            foreach (var i in bfp.PathTo(4))
-            {
-                Console.WriteLine(i);
-            }
-            */
-            /*
-            Console.WriteLine(bfp.DistanceTo(4));
-            Console.WriteLine(bfp.DistanceTo(5));
-            Console.WriteLine(bfp.DistanceTo(3));
-            */
+            Console.WriteLine("Ordered keys: ");
+            foreach (var k in st.Keys())
+                Console.WriteLine(k);
 
             Console.ReadKey();
         }
