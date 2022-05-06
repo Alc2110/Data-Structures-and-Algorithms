cb9ea1e8c47abc1c374f267ae918e4f4c790ba5d
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-05-06 11:44:51 +1000

    add C# implementation of edge-weighted directed graph

---
+++ CsImplementations/CsImplementations/Graphs/EdgeWeightedGraphs/DirectedEdge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Graphs.EdgeWeightedGraphs
+{
+    public class DirectedEdge
+    {
+        private readonly int _v; // tail
+        private readonly int _w; // head
+        private readonly double _weight;
+
+        public DirectedEdge(int v, int w, double weight)
+        {
+            this._v = v;
+            this._w = w;
+            this._weight = weight;
+        }//ctor
+
+        public double Weight
+        {
+            get => this._weight;
+        }
+
+        public int From() => this._v;
+
+        public int To() => this._w;
+
+        public override string ToString()
+        {
+            return (this._v + ", " + this._w + ", " + this._weight);
+        }
+    }//class
+}
---
+++ CsImplementations/CsImplementations/Graphs/EdgeWeightedGraphs/EdgeWeightedDigraph.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Graphs.EdgeWeightedGraphs
+{
+    public class EdgeWeightedDigraph
+    {
+        private readonly int _v; // number of vertices
+        private int _e; // number of edges
+        private List<DirectedEdge>[] _adj; // adjacency lists
+
+        public EdgeWeightedDigraph(int vertices)
+        {
+            this._v = vertices;
+            this._e = 0;
+            this._adj = new List<DirectedEdge>[this._v];
+        }//ctor
+
+        public int NumberOfVertices
+        {
+            get => this._v;
+        }
+
+        public int NumberOfEdges
+        {
+            get => this._e;
+        }
+
+        public void AddEdge(DirectedEdge edge)
+        {
+            this._adj[edge.From()].Add(edge);
+            this._e++;
+        }
+
+        public IEnumerable<DirectedEdge> Adjacency(int v)
+        {
+            return this._adj[v];
+        }
+
+        public IEnumerable<DirectedEdge> Edges()
+        {
+            List<DirectedEdge> edges = new List<DirectedEdge>();
+            for (int v = 0; v < this._v; v++)
+            {
+                foreach (DirectedEdge e in this._adj[v])
+                {
+                    edges.Add(e);
+                }
+            }
+
+            return edges;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder graphString = new StringBuilder();
+            foreach (var e in this.Edges())
+            {
+                graphString.AppendLine(e.ToString());
+            }
+
+            return graphString.ToString();
+        }
+    }//class
+}
